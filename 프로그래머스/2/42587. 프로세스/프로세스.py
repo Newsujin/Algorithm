884929e534c3577eaa09c8a52cf322604157da91
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-01-10 23:33:37 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -3,22 +3,17 @@ from collections import deque
 def solution(priorities, location):
     queue = deque()
     exec_cnt = 0
-
     for i in range(len(priorities)):
         queue.append((priorities[i], i))
-
     while queue:
         current_value = queue.popleft()
-
-        # 큐에 남아 있는 프로세스들과 비교
         for i in range(len(queue)):
-            if current_value[0] < queue[i][0]:
-                queue.append(current_value)
-                break  # 우선순위가 더 높은 프로세스가 있으면 현재 프로세스를 큐에 넣고 중단
+            if (current_value[0] < queue[i][0]):
+                    queue.append(current_value)
+                    break
         else:
             exec_cnt += 1
             if current_value[1] == location:
-                return exec_cnt
+                 return exec_cnt
 
-# 예제 실행
 print(solution([2, 1, 3, 2], 3))
