e2504bc24f53824b9963a72bac702655f5743180
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-01-20 23:51:10 +0900

    [level 1] Title: 모의고사, Time: 2.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,13 +3,11 @@ def solution(answers):
     cnt = [0, 0, 0]
 
     for i, val in enumerate(answers):
-        if students[0][(i + 1) % 5 - 1] == val:
+        if students[0][i % 5] == val:
             cnt[0] += 1
-        if students[1][(i + 1) % 8 - 1] == val:
+        if students[1][i % 8] == val:
             cnt[1] += 1
-        if students[2][(i + 1) % 10 - 1] == val:
+        if students[2][i % 10] == val:
             cnt[2] += 1
     max_val = max(cnt)
     return [idx + 1 for idx, val in enumerate(cnt) if val == max_val]
-
-print(solution([1,3,2,4,2]))
