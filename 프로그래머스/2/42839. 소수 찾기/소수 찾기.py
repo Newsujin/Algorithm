f651d10015c92b22584a5d54be07a804a9cea61f
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-01-22 00:02:29 +0900

    [level 2] Title: 소수 찾기, Time: 0.18 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,30 +1,23 @@
 from itertools import permutations
 
-# 에라토스테네스의 체
-def prime_list(n):
-    prime = [True] * (n + 1)
-    prime[0] = prime[1] = False 
-    
-    for i in range(2, int(n ** 0.5) + 1):
-        if prime[i]:
-            for j in range(i*i, len(prime), i):
-                prime[j] = False
-    return prime
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
     
 def solution(numbers):
     numbers = list(numbers)
     number_set = set()
+    cnt = 0
     
-    # 가능한 모든 경우의 수 (순열) => set 사용해서 중복 제거
     for i in range(1, len(numbers) + 1):
         number_set |= set(map(int, map(''.join, permutations(numbers, i))))
     
-    # 에라토스테네스의 체로 소수 판별 리스트 생성
-    prime = prime_list(max(number_set))
+    for num in number_set:
+        if is_prime(num):
+            cnt += 1
     
-    # 소수 판별
-    answer = 0
-    for number in number_set:
-        if prime[number]:
-            answer += 1
-    return answer
+    return cnt
