84dde7e4041c244ac1637908c168ea6b2990f2d1
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-02-18 16:40:10 +0900

    [Silver IV] Title: 반복수열, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2331. 반복수열/반복수열.py
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -1,14 +1,9 @@
-def solution(A, P):
-    answer = [A]
-    cur_n = A
-    repeat_start_i = -1
-    while repeat_start_i == -1:
-        cur_n = sum(int(n) ** P for n in str(cur_n))
-        if cur_n in answer:
-            repeat_start_i = answer.index(cur_n)
-        answer.append(cur_n)
-    print(repeat_start_i)
-    return (repeat_start_i)
-
 A, P = map(int, input().split())
-solution(A, P)
+answer = [A]
+cur_n = A
+while True:
+    cur_n = sum(int(n) ** P for n in str(cur_n))
+    if cur_n in answer:
+        break
+    answer.append(cur_n)
+print(answer.index(cur_n))
