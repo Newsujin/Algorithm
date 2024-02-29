8ac144e02d1a8c41c7710fe31dd4ded9ff704b61
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-02-29 17:13:55 +0900

    Delete 프로그래머스/Lv.1 directory

--- 프로그래머스/Lv.1/1845. 폰켓몬/폰켓몬.py
+++
@@ -1,7 +0,0 @@
-def solution(nums):
-    answer = set(nums)
-    n = len(answer)
-    if (n < len(nums) / 2):
-        return (n)
-    else:
-        return len(nums) / 2
--- 프로그래머스/Lv.1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter
-
-def solution(participant, completion):
-    target = Counter(participant) - Counter(completion)
-    answer = list(target.keys())[0]
-    return answer
