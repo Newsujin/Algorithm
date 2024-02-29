811b70d6282897ca0333ed158b7b5550141f5b92
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-02-29 17:36:01 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,5 +1,4 @@
 def solution(citations):
-	# citations.sort()
 	m = 0
 	for val in citations:
 		cnt = 0
@@ -7,8 +6,5 @@ def solution(citations):
 			if val <= citations[i]:
 				cnt += 1
 		if val >= cnt:
-			val = cnt
-		if m < val:
-			m = val
-			# m = max(m, cnt)
+			m = max(m, cnt)
 	return m
