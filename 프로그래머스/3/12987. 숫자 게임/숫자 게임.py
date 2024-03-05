58c8102ae86979760f45e1ead1aa3cac5f72c883
Newsujin <104690611+Newsujin@users.noreply.github.com>
2024-03-05 15:30:50 +0900

    [level 3] Title: 숫자 게임, Time: 643.15 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -3,9 +3,7 @@ def solution(A, B):
     B.sort(reverse=True)
     answer = 0
     for a in A:
-        if a >= B[0]:
-            continue
-        else:
+        if a < B[0]:
             del B[0]
             answer += 1
     return (answer)
