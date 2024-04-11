ed93c4d748b78e7efcfa1dd51fe29bff2cccf519
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-04-11 14:38:17 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 472 ms, Memory: 39668 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -4,26 +4,33 @@ from collections import deque
 n, m = map(int, sys.stdin.readline().split())
 maps = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
 
+
 visited = [[False] * m for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x, y):
-    visited[x][y] = 1
-    maps[x][y] = 0
-    queue = deque()
-    queue.append((x, y))
 
-    while queue:
-        x, y = queue.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0 <= nx < n and 0 <= ny <m and maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                queue.append((nx, ny))
-                maps[nx][ny] = maps[x][y] + 1
+def bfs(y, x):
+    maps[y][x] = 0
+    visited[y][x] = True
+    q = deque([(x, y, 1)])
+    
+    while q:
+        x, y, distance = q.popleft()
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1 and not visited[ny][nx]:
+                maps[ny][nx] = distance
+                visited[ny][nx] = True
+                q.append((nx, ny, distance + 1))
 
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j] and maps[i][j] == 1:
+                maps[i][j] = -1
+
+    for line in maps:
+        print(*line)
 
 for i in range(n):
     for j in range(m):
@@ -32,11 +39,3 @@ for i in range(n):
             break
 
 bfs(x, y)
-
-for i in range(n):
-    for j in range(m):
-        if maps[i][j] == 1 and visited[i][j] == 0:
-            maps[i][j] = -1
-
-for line in maps:
-    print(*line)
