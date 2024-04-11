a6012dc5cee4f1da8a99176eb7f122fe3f13feba
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-04-11 14:30:44 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 476 ms, Memory: 39664 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -1,10 +1,16 @@
+import sys
 from collections import deque
 
+n, m = map(int, sys.stdin.readline().split())
+maps = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+
+visited = [[False] * m for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 def bfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     visited[x][y] = 1
-    graph[x][y] = 0
+    maps[x][y] = 0
     queue = deque()
     queue.append((x, y))
 
@@ -13,24 +19,15 @@ def bfs(x, y):
         for k in range(4):
             nx = x + dx[k]
             ny = y + dy[k]
-            if 0 <= nx < n and 0 <= ny <m and graph[nx][ny] == 1 and visited[nx][ny] == 0:
+            if 0 <= nx < n and 0 <= ny <m and maps[nx][ny] == 1 and visited[nx][ny] == 0:
                 visited[nx][ny] = 1
                 queue.append((nx, ny))
-                graph[nx][ny] = graph[x][y] + 1
+                maps[nx][ny] = maps[x][y] + 1
 
-n, m = map(int, input().split())
-x, y = 0, 0
-
-visited = [[0 for _ in range(m)] for _ in range(n)]
-graph = []
-
-for i in range(n):
-    x = list(map(int, input().split()))
-    graph.append(x)
 
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 2:
+        if maps[i][j] == 2:
             x, y = i, j
             break
 
@@ -38,8 +35,8 @@ bfs(x, y)
 
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1 and visited[i][j] == 0:
-            graph[i][j] = -1
+        if maps[i][j] == 1 and visited[i][j] == 0:
+            maps[i][j] = -1
 
-for line in graph:
+for line in maps:
     print(*line)
