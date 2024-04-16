bd42bbae270e8d7062382132c19bbf0f510078a2
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-04-16 14:49:41 +0900

    [level 2] Title: 광물 캐기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -1,10 +1,6 @@
-from collections import deque
-
 def solution(picks, minerals):
-	# 광물 5개씩 끊기
 	divided_minerals = [minerals[i:i + 5] for i in range(0, min(sum(picks) * 5, len(minerals)), 5)]
 
-	# 각 묶음에서 곡괭이 별로 필요한 피로도 계산
 	fatigues = []
 	for part in divided_minerals:
 		fatigue = [0, 0, 0]
@@ -13,12 +9,9 @@ def solution(picks, minerals):
 			fatigue[1] += 5 if mineral == "diamond" else 1
 			fatigue[2] += 25 if mineral == "diamond" else 5 if mineral == "iron" else 1
 		fatigues.append(fatigue)
-	print(fatigues)
 
-	# 낮은 등급의 곡괭이 점수를 기준으로 내림차순 정렬
-	fatigues.sort(key=lambda x:(-x[2], -x[1], x[0]))
+	fatigues.sort(key=lambda x:(-x[2], -x[1], -x[0]))
 
-	# 다이아몬드 곡괭이부터 순서대로 사용
 	ans = 0
 	for part in fatigues:
 		if picks[0]:
