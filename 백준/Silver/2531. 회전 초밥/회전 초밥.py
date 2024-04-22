37082b424d4d7716f88466315bc3d61e4c491d29
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-04-22 12:45:21 +0900

    [Silver I] Title: 회전 초밥, Time: 3260 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.py
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -5,8 +5,8 @@ sushi_belt = [int(sys.stdin.readline().strip()) for _ in range(n)]
 
 max_num = 0
 for i in range(n):
-    if i + k >= n:
-        num = set(sushi_belt[i:] + sushi_belt[:(i + k) % n] + [c])
+    if i + k > n:
+        num = set(sushi_belt[i:] + sushi_belt[:(i + k) - n] + [c])
     else:
         num = set(sushi_belt[i:i + k] + [c])
     max_num = max(max_num, len(num))
