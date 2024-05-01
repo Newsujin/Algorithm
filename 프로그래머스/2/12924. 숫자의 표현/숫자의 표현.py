6bf3003cbcde029864e8934f8f33d3882aa64dbd
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-05-01 12:21:51 +0900

    [level 2] Title: 숫자의 표현, Time: 1.41 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,11 +1,18 @@
 def solution(n):
 	cnt = 0
-	for i in range(1, n + 1):
-		tmp = 0
-		for j in range(i, n + 1):
-			tmp += j
-			if tmp >= n:
-				if tmp == n:
-					cnt += 1
-				break
+	start = 1
+	end = 1
+	n_sum = 1
+
+	while end <= n:
+		if n_sum == n:
+			cnt += 1
+			n_sum -= start
+			start += 1
+		elif n_sum < n:
+			end += 1
+			n_sum += end
+		else:
+			n_sum -= start
+			start += 1
 	return cnt
