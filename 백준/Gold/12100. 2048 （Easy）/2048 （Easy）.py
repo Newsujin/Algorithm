49e5e330b9ce8b6128b3c4264a8168790a1b0317
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-05-19 22:55:11 +0900

    [Gold II] Title: 2048 (Easy), Time: 368 ms, Memory: 31836 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -10,82 +10,86 @@ ans = 0
 def move_left(board):
     for i in range(n):
         cursor = 0
-        for j in range(1, n):
-            if board[i][j] != 0: # 0이 아닌 값이
+        for j in range(n):
+
+            if board[i][j]:
                 tmp = board[i][j]
-                board[i][j] = 0 # 일단 비워질꺼니까 0으로 바꿈
+                board[i][j] = 0
 
-                if board[i][cursor] == 0: # 비어있으면
-                    board[i][cursor] = tmp # 옮긴다
-
-                elif board[i][cursor] == tmp: # 같으면
-                    board[i][cursor] *= 2 # 합친다
+                if not board[i][cursor]:
+                    board[i][cursor] = tmp
+                elif board[i][cursor] == tmp:
+                    board[i][cursor] *= 2
                     cursor += 1
-                else: # 비어있지도 않고 다른 값일때
-                    cursor += 1 # pass
-                    board[i][cursor] = tmp # 바로 옆에 붙임
-
+                else:
+                    cursor += 1
+                    board[i][cursor] = tmp
+    
     return board
+
 
 def move_right(board):
     for i in range(n):
         cursor = n - 1
         for j in range(n - 1, -1, -1):
 
-            if board[i][j] != 0:
+            if board[i][j]:
                 tmp = board[i][j]
                 board[i][j] = 0
 
-                if board[i][cursor] == 0:
+                if not board[i][cursor]:
                     board[i][cursor] = tmp
-
                 elif board[i][cursor] == tmp:
                     board[i][cursor] *= 2
                     cursor -= 1
                 else:
                     cursor -= 1
                     board[i][cursor] = tmp
+
     return board
+
 
 def move_up(board):
     for j in range(n):
         cursor = 0
         for i in range(n):
-            if board[i][j] != 0:
+
+            if board[i][j]:
                 tmp = board[i][j]
                 board[i][j] = 0
 
-                if board[cursor][j] == 0:
+                if not board[cursor][j]:
                     board[cursor][j] = tmp
-
                 elif board[cursor][j] == tmp:
                     board[cursor][j] *= 2
                     cursor += 1
-
                 else:
                     cursor += 1
                     board[cursor][j] = tmp
+                    
     return board
+
 
 def move_down(board):
     for j in range(n):
         cursor = n - 1
         for i in range(n - 1, -1, -1):
-            if board[i][j] != 0:
+
+            if board[i][j]:
                 tmp = board[i][j]
                 board[i][j] = 0
 
-                if board[cursor][j] == 0:
+                if not board[cursor][j]:
                     board[cursor][j] = tmp
-
                 elif board[cursor][j] == tmp:
                     board[cursor][j] *= 2
                     cursor -= 1
-
                 else:
                     cursor -= 1
                     board[cursor][j] = tmp
+                    
     return board
+
 
 def dfs(board, cnt):
     global ans
