b54ce84850d49d4229f0bdac955c3b54e0b2d16c
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-05-19 22:42:58 +0900

    [Gold II] Title: 2048 (Easy), Time: 376 ms, Memory: 31872 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -0,0 +1,111 @@
+import sys
+from copy import deepcopy
+
+
+n = int(sys.stdin.readline())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+ans = 0
+
+
+def move_left(board):
+    for i in range(n):
+        cursor = 0
+        for j in range(1, n):
+            if board[i][j] != 0: # 0이 아닌 값이
+                tmp = board[i][j]
+                board[i][j] = 0 # 일단 비워질꺼니까 0으로 바꿈
+
+                if board[i][cursor] == 0: # 비어있으면
+                    board[i][cursor] = tmp # 옮긴다
+
+                elif board[i][cursor] == tmp: # 같으면
+                    board[i][cursor] *= 2 # 합친다
+                    cursor += 1
+                else: # 비어있지도 않고 다른 값일때
+                    cursor += 1 # pass
+                    board[i][cursor] = tmp # 바로 옆에 붙임
+
+    return board
+
+def move_right(board):
+    for i in range(n):
+        cursor = n - 1
+        for j in range(n - 1, -1, -1):
+
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[i][cursor] == 0:
+                    board[i][cursor] = tmp
+
+                elif board[i][cursor] == tmp:
+                    board[i][cursor] *= 2
+                    cursor -= 1
+                else:
+                    cursor -= 1
+                    board[i][cursor] = tmp
+    return board
+
+def move_up(board):
+    for j in range(n):
+        cursor = 0
+        for i in range(n):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor += 1
+
+                else:
+                    cursor += 1
+                    board[cursor][j] = tmp
+    return board
+
+def move_down(board):
+    for j in range(n):
+        cursor = n - 1
+        for i in range(n - 1, -1, -1):
+            if board[i][j] != 0:
+                tmp = board[i][j]
+                board[i][j] = 0
+
+                if board[cursor][j] == 0:
+                    board[cursor][j] = tmp
+
+                elif board[cursor][j] == tmp:
+                    board[cursor][j] *= 2
+                    cursor -= 1
+
+                else:
+                    cursor -= 1
+                    board[cursor][j] = tmp
+    return board
+
+def dfs(board, cnt):
+    global ans
+    if cnt == 5:
+        for i in range(n):
+            for j in range(n):
+                ans = max(ans, board[i][j])
+        return
+    
+    for i in range(4):
+        tmp_board = deepcopy(board)
+        if i == 0:
+            dfs(move_left(tmp_board), cnt + 1)
+        elif i == 1:
+            dfs(move_right(tmp_board), cnt + 1)
+        elif i == 2:
+            dfs(move_up(tmp_board), cnt + 1)
+        else:
+            dfs(move_down(tmp_board), cnt + 1)
+
+
+dfs(board, 0)
+print(ans)
