c3ceed40c9ec3a669103cbf16ab7a397fbab5060
spark2 <104690611+Newsujin@users.noreply.github.com>
2024-05-29 22:13:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 108 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -5,21 +5,20 @@ dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
 def dijkstra():
-    move[0][0] = 0
     q = []
     heapq.heappush(q, [cave[0][0], 0, 0])
 
     while q:
         coin_sum, x, y = heapq.heappop(q)
         if x == n - 1 and y == n - 1:
-            print(f'Problem {test_cnt}: {move[y][x]}')
+            print(f'Problem {test_cnt}: {cost[y][x]}')
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and move[ny][nx] > cave[ny][nx] + coin_sum:
-                move[ny][nx] = cave[ny][nx] + coin_sum
-                heapq.heappush(q, [move[ny][nx], nx, ny])
+            if 0 <= nx < n and 0 <= ny < n and cost[ny][nx] > cave[ny][nx] + coin_sum:
+                cost[ny][nx] = cave[ny][nx] + coin_sum
+                heapq.heappush(q, [cost[ny][nx], nx, ny])
 
 
 test_cnt = 1
@@ -28,7 +27,7 @@ while (True):
     if n == 0:
         break
     cave = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-    move = [[int(10e9)] * n for _ in range(n)]
+    cost = [[int(10e9)] * n for _ in range(n)]
 
     dijkstra()
     test_cnt += 1
