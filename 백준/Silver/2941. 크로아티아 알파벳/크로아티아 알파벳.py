b11697d4d664d9930d7daddd1627fac777c3dbb1
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-01-20 20:12:39 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -2,7 +2,5 @@ croatian = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 s = input()
 
 for pattern in croatian:
-	if pattern in s:
-		n = s.count(pattern)
-		s = s.replace(pattern, '*', n)
+	s = s.replace(pattern, '*')
 print(len(s))
