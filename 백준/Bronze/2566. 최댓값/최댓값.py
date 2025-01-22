b3dc3a63196865a4a3881b42d4f674df83f59045
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-01-22 16:18:53 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -3,16 +3,14 @@ import sys
 board = []
 for i in range(9):
     board.append(list(map(int, sys.stdin.readline().split())))
-    max_n = max(board[i])
 
 max_value = -1
-row = 0
-col = 0
+row, col = 0, 0
 for i in range(9):
     for j in range(9):
         if board[i][j] > max_value:
             max_value = board[i][j]
-            row = i
-            col = j
+            row, col = i, j
+
 print(max_value)
 print(row + 1, col + 1)
