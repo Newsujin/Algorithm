981f3c563caf27dcb45e52781329b41fee1e3b6b
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-04-17 23:20:21 +0900

    [level 2] Title: 문자열 압축, Time: 4.42 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,33 +1,20 @@
-def compress_str(s, unit):
-    compressed = ""
-    prev = s[:unit]
-    cnt = 1
-
-    for i in range(unit, len(s), unit):
-        current = s[i:i + unit]
-        if current == prev:
-            cnt += 1
-        else:
-            if cnt > 1:
-                compressed += str(cnt) + prev
-            else:
-                compressed += prev
-            prev = current
-            cnt = 1
-
-    if cnt > 1:
-        compressed += str(cnt) + prev
-    else:
-        compressed += prev
-    
-    return compressed
-
-
 def solution(s):
-    min_len = len(s)
-    if min_len == 1:
-        return 1
-    for unit in range(1, min_len // 2 + 1):
-        compressed = compress_str(s, unit)
-        min_len = min(min_len, len(compressed))
-    return min_len
+    answer = len(s)
+    
+    for unit in range(1, len(s) // 2 + 1):
+        compressed = ''
+        repeat_s = s[:unit]
+        cnt = 1
+        
+        for i in range(unit, len(s), unit):
+            if repeat_s == s[i:i + unit]:
+                cnt += 1
+            else:
+                compressed += str(cnt) + repeat_s if cnt > 1 else repeat_s
+                cnt = 1
+                repeat_s = s[i:i + unit]
+        
+        compressed += str(cnt) + repeat_s if cnt > 1 else repeat_s
+        answer = min(answer, len(compressed))
+    
+    return answer
