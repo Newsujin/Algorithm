758775a4d755090510192a3a4e0b94ba4631579b
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-05-09 01:24:53 +0900

    [level 2] Title: 더 맵게, Time: 213.16 ms, Memory: 41.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 205.35 ms
+메모리: 41.2 MB, 시간: 213.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 09일 01:12:10
+2025년 05월 09일 01:24:51
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -1,32 +1,23 @@
-#include <algorithm>
-#include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    make_heap(scoville.begin(), scoville.end(), greater<int>());
-
-    while (scoville.size() > 1) {
-        int first = scoville.front();
-        if (first >= K)
-            break;
-        pop_heap(scoville.begin(), scoville.end(), greater<int>());
-        scoville.pop_back();
-        
-        int second = scoville.front();
-        pop_heap(scoville.begin(), scoville.end(), greater<int>());
-        scoville.pop_back();
-
-        scoville.push_back(first + second * 2);
-        push_heap(scoville.begin(), scoville.end(), greater<int>());
-        
+    priority_queue<int, vector<int>, greater<int> > q;
+    for (int i = 0; i < scoville.size(); i++)
+        q.push(scoville[i]);
+    
+    while (q.size() > 1 && q.top() < K) {
         answer++;
+        int a = q.top();
+        q.pop();
+        int b = q.top();
+        q.pop();
+        q.push(a + b * 2);
     }
-    if (scoville.front() < K) {
-        return -1;
-    }
+    if (q.top() < K) return -1;
     return answer;
 }
