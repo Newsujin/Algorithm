9c48fb18e7244dac7712c42bdc956105d8728892
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-05-10 02:30:57 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.02 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,21 +1,19 @@
 from collections import deque
 
 def solution(maps):
-	answer = 0
-	dx = [-1, 1, 0, 0]
-	dy = [0, 0, -1, 1]
-	queue = deque()
-	queue.append((0, 0))
-		
-	while queue:
-		x, y = queue.popleft()
-		for i in range(4):
-			nx = x + dx[i]
-			ny = y + dy[i]
-			h = len(maps)
-			w = len(maps[0])
-			if 0 <= nx < h and 0 <= ny < w and maps[nx][ny] == 1:
-				maps[nx][ny] = maps[x][y] + 1
-				queue.append((nx, ny))
-	answer = maps[h - 1][w -1]
-	return answer if answer != 1 else -1
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+    
+    q = deque([(0, 0)])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1):
+                maps[nx][ny] = maps[x][y] + 1
+                if (nx == len(maps) - 1 and ny == len(maps[0]) - 1):
+                    return maps[nx][ny]
+                q.append((nx, ny))
+
+    return -1
