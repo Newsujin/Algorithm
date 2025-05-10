fd1c627650bc8cebf02e79499fe84a7f14b68900
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-05-10 14:48:58 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -1,35 +1,37 @@
 cash = int(input())
 stock_prices = list(map(int, input().split()))
 
-jh_cash = cash
-sm_cash = cash
-jh_cnt = 0
-sm_cnt = 0
-decrease = 0
-increase = 0
+jh_cash = sm_cash = cash
+jh_cnt = sm_cnt = up = down = 0
 
 for idx, price in enumerate(stock_prices):
-    if price <= jh_cash:
-        jh_cnt += jh_cash // price
-        jh_cash = jh_cash % price
+    # 준현
+    jh_cnt += jh_cash // price
+    jh_cash = jh_cash % price
+    
+    # 성민
     if idx > 0:
-        if stock_prices[idx - 1] > stock_prices[idx]: # 하락
-            increase = 0
-            decrease += 1
-            if decrease >= 3 and price < sm_cash: # 3일 연속 하락
-                sm_cnt += sm_cash // price
-                sm_cash = sm_cash % price
-        elif stock_prices[idx - 1] < stock_prices[idx]: # 상승
-            decrease = 0
-            increase += 1
-            if increase >= 3 and sm_cnt != 0: # 3일 연속 상승
-                sm_cash += sm_cnt * price
-                sm_cnt = 0
+        today, yesterday = stock_prices[idx], stock_prices[idx - 1]
+        if yesterday > today: # 하락
+            up = 0
+            down += 1
+        elif yesterday < today: # 상승
+            down = 0
+            up += 1
+        if down >= 3: # 3일 연속 하락
+            sm_cnt += sm_cash // price
+            sm_cash = sm_cash % price
+        if up >= 3 and sm_cnt > 0: # 3일 연속 상승
+            sm_cash += sm_cnt * price
+            sm_cnt = 0
 
-last_price = stock_prices[len(stock_prices) - 1]
-if (last_price * jh_cnt + jh_cash > last_price * sm_cnt + sm_cash):
+last_price = stock_prices[-1]
+jh_total = last_price * jh_cnt + jh_cash
+sm_total = last_price * sm_cnt + sm_cash
+
+if (jh_total > sm_total):
     print("BNP")
-elif (last_price * jh_cnt + jh_cash < last_price * sm_cnt + sm_cash):
+elif (jh_total < sm_total):
     print("TIMING")
 else:
     print("SAMESAME")
