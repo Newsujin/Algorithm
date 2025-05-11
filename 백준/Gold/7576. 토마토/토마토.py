96ef7dd4cef9e3ffd9a3348346db7fffab042f18
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-05-11 18:11:28 +0900

    [Gold V] Title: 토마토, Time: 1304 ms, Memory: 100608 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,30 +1,35 @@
 from collections import deque
 
 m, n = map(int, input().split())
-box = [list(map(int, input().split())) for _ in range (n)]
+box = []
+q = deque()
+for i in range(n):
+    row = list(map(int, input().split()))
+    box.append(row)
+    for j in range(m):
+        if row[j] == 1:
+            q.append((i, j))
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-ripe_tomato = deque()
-for i in range(n):
-    for j in range(m):
-        if box[i][j] == 1:
-            ripe_tomato.append((i, j))
-
-day = -1
-while ripe_tomato:
-    for _ in range(len(ripe_tomato)):
-        x, y = ripe_tomato.popleft()
+def bfs():
+    while (q):
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            
             if 0 <= nx < n and 0 <= ny < m and box[nx][ny] == 0:
-                box[nx][ny] = 1
-                ripe_tomato.append((nx, ny))
-    day += 1
+                box[nx][ny] = box[x][y] + 1
+                q.append((nx, ny))
 
-if all(all(cell != 0 for cell in row) for row in box):
-    print(day)
-else:
-    print(-1)
+bfs()
+res = 0
+for row in box:
+    if 0 in row:
+        res = -1
+        break
+    res = max(res, max(row))
+
+print(res - 1 if res != -1 else -1)
