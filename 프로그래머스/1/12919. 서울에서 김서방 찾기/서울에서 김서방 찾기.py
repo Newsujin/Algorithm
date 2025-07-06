2e0f4f72aeeccbb6eec972c34efd62ac0be8beec
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-07-06 20:11:36 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,7 +1,4 @@
 def solution(seoul):
-    
-    answer = ''
-    
-    answer = "김서방은 {}에 있다".format(seoul.index('Kim'))
-    
-    return answer
+    for i in range(len(seoul)):
+        if seoul[i] == 'Kim':
+            return f"김서방은 {i}에 있다"
