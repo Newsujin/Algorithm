67a141d40bb79e0e2789b41f11116deb81bfa127
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-07-10 15:05:51 +0900

    [level 1] Title: 바탕화면 정리, Time: 1.05 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,11 +1,15 @@
 def solution(wallpaper):
-    
-    x, y = [], []
+    min_x = 50
+    min_y = 50
+    max_x = 0
+    max_y = 0
     
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                x.append(i)
-                y.append(j)
+                min_x = min(min_x, i)
+                min_y = min(min_y, j)
+                max_x = max(max_x, i)
+                max_y = max(max_y, j)
     
-    return [min(x), min(y), max(x) + 1, max(y) + 1]
+    return [min_x, min_y, max_x + 1, max_y + 1]
