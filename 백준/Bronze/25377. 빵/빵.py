6d83fcca2163b6530a2816a44d61291784feeb4d
spark2 <104690611+Newsujin@users.noreply.github.com>
2026-04-27 20:40:44 +0900

    [Bronze IV] Title: 빵, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,12 +1,12 @@
-from sys import stdin
-n = int(stdin.readline().strip())
-result = 1001
-for i in range(n):
-    a, b = map(int, stdin.readline().split())
-    if b>=a:
-        if b<result:
-            result = b
-if result == 1001:
+N = int(input())
+min_s = 1001
+
+for _ in range(N):
+    A, B = map(int, input().split())
+    if A <= B:
+        min_s = min(min_s, B)
+
+if min_s == 1001:
     print(-1)
 else:
-    print(result)
+    print(min_s)
